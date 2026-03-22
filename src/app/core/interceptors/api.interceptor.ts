8adc833a13e0ad918940b803543278405a834075
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2026-03-22 13:00:54 +0100

    mv environments

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { environment } from "src/app/environments/environment";
+import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
